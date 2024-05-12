2d969854457170a58f1512d27d17596d9b11d9d9
VitaBabenko <babenkovita1988@gmail.com>
2024-05-13 00:12:51 +0300

    done adaptive for mobile and tablet

--- src/layout/Footer/Footer.tsx
+++ src/layout/Footer/Footer.tsx
@@ -3,18 +3,18 @@ import Link from 'next/link';
 
 export const Footer: React.FC = () => {
   return (
-    <footer className="section bg-blue-600">
-      <div className="container">
-        <div className="flex items-center justify-between">
+    <footer className="bg-blue-600 py-4 xl:relative xl:py-6">
+      <div className="container flex flex-col gap-4 md:flex-row md:justify-between">
+        <div className="flex flex-col gap-4 xl:flex-row xl:items-center">
           <Logo />
           <Link
             href="mailto:wds.webdevsynergy@gmail.com"
-            className="text-[20px] text-white transition-all hover:text-accent"
+            className="text-[20px] text-white transition-all hover:text-accent xl:absolute xl:left-[50%] xl:translate-x-[-50%]"
           >
             wds.webdevsynergy@gmail.com
           </Link>
-          <Contacts />
         </div>
+        <Contacts />
       </div>
     </footer>
   );
--- src/sections/ServicesSection/ServicesSection.tsx
+++ src/sections/ServicesSection/ServicesSection.tsx
@@ -5,45 +5,44 @@ import ServicesImg from '~/images/services.jpeg';
 export const ServicesSection: React.FC = () => {
   return (
     <section className="section bg-bgSecondary">
-      <div className="container">
-        <div className="flex gap-24">
-          <Image
-            src={ServicesImg}
-            width="600"
-            height="600"
-            alt="вебсайти на різних пристроях"
-          />
-          <div className="w-full">
-            <h2 className="mb-12 text-[40px] font-semibold uppercase tracking-[2px]">
-              Що ми пропонуємо
-            </h2>
-            <ul className="list-marker text-[24px] font-medium leading-[2]">
-              <li>
-                <p>Розробка дизайну</p>
-              </li>
-              <li>
-                <p>Розробка лендінгів (сайт-візитка)</p>
-              </li>
-              <li>
-                <p>Розробка листівок</p>
-              </li>
-              <li>
-                <p>Розробка багатосторінкових сайтів</p>
-              </li>
-              <li>
-                <p>Розробка веб-застосунків</p>
-              </li>
-              <li>
-                <p>Розробка застосунків для мобільних пристроїв</p>
-              </li>
-              <li>
-                <p>Редизайн та рефакторінг</p>
-              </li>
-              <li>
-                <p>Інші види робіт за домовленістю</p>
-              </li>
-            </ul>
-          </div>
+      <div className="container xl:flex xl:items-center xl:justify-between">
+        <Image
+          src={ServicesImg}
+          width="500"
+          height="500"
+          alt="вебсайти на різних пристроях"
+          className="mdOnly:mx-auto"
+        />
+        <div>
+          <h2 className="mb-5 text-3xl font-semibold uppercase tracking-[2px] md:text-4xl">
+            Що ми пропонуємо
+          </h2>
+          <ul className="list-marker pl-4 text-[16px] font-medium leading-[2] md:text-xl">
+            <li>
+              <p>Розробка дизайну</p>
+            </li>
+            <li>
+              <p>Розробка лендінгів (сайт-візитка)</p>
+            </li>
+            <li>
+              <p>Розробка листівок</p>
+            </li>
+            <li>
+              <p>Розробка багатосторінкових сайтів</p>
+            </li>
+            <li>
+              <p>Розробка веб-застосунків</p>
+            </li>
+            <li>
+              <p>Розробка застосунків для мобільних пристроїв</p>
+            </li>
+            <li>
+              <p>Редизайн та рефакторінг</p>
+            </li>
+            <li>
+              <p>Інші види робіт за домовленістю</p>
+            </li>
+          </ul>
         </div>
       </div>
     </section>
